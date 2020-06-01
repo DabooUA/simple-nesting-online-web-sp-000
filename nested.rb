dd080df79f22a29027a17bc19b2f9bc7f24b757d
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-01 20:45:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,8 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     newly_added_to_hash = "Assembly"
-     programmer_hash[:dennis_ritchie][:languages] = newly_added_to_hash
-     programmer_hash
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash[:dennis_ritchie][:languages]
 
 end
